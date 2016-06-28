4b1a874a5e33cd0686d446808f97b2969f166141
Ryan Flach <ryanflach@users.noreply.github.com>
2016-06-28 12:34:57 -0600

    Add logic for printing out author names and e-mails, auto clone and clean-up of repo

--- lib/okcugit.rb
+++ lib/okcugit.rb
@@ -1,25 +1,44 @@
-
 module OkCuGit
   class Finder
-    attr_reader :repo_name
+    attr_reader :user_and_repo,
+                :repo_name
 
-    def initialize(repo_name)
-      @repo_name = repo_name
+    def initialize(user_and_repo)
+      @user_and_repo = user_and_repo
+      @repo_name = user_and_repo.split("/")[1]
+    end
+
+    def clone_repo
+      Dir.chdir("lib")
+      `git clone https://github.com/"#{user_and_repo}"`
+      Dir.chdir(repo_name)
     end
 
     def all_contributors
-      directory = repo_name.split('/').last
-      Dir.chdir(directory)
       log = `git log`
-      array = log.split("\n")
-      # logic that ryan built will go here 
-      # print log
+      all_authors_and_emails = log.split("\n").select do |line|
+        line.include?("<")
+      end.map { |line| line[8..-1] }
+
+      unique_authors = all_authors_and_emails.map do |line|
+        if line[0].downcase == line[0]
+          author = line.split(" ")
+        else
+          author = line.downcase.split(" ").map {|name| name.capitalize}
+        end
+        author.join(" ")
+      end.uniq.sort_by { |name| name[0].downcase }
+    end
+
+    def clean_up
+      Dir.chdir('..')
+      `rm -rf #{repo_name}`
     end
   end
 end
 
-repo_name = ARGV[0]
-finder = OkCuGit::Finder.new(repo_name)
-# print log
-# contacts = finder.all_contributors
-# puts contacts.join("\n")
+user_and_repo = ARGV[0]
+finder = OkCuGit::Finder.new(user_and_repo)
+finder.clone_repo
+puts finder.all_contributors
+finder.clean_up
